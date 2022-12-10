ddeb78e55be111c5b3d9608c44d773665feb4e57
Leonardo Santo <leonard_sc@hotmail.com>
2022-12-10 16:00:53 -0500

    primer comit

---
+++ src/test/java/ec/edu/itsqmet/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
