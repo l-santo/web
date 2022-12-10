787df420eff47940fed6221b57de833d0eb0ef47
Leonardo Santo <leonard_sc@hotmail.com>
2022-12-10 17:09:07 -0500

    AplicationTest

--- src/test/java/ec/edu/itsqmet/web/WebApplicationTests.java
+++ src/test/java/ec/edu/itsqmet/web/WebApplicationTests.java
@@ -1,13 +1,19 @@
 package ec.edu.itsqmet.web;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ec.edu.itsqmet.web.repository.IWebRepository;
 
 @SpringBootTest
 class WebApplicationTests {
+	@Autowired
+	private IWebRepository webRepository;
 
 	@Test
 	void contextLoads() {
+		System.out.println(webRepository.count());
 	}
 
 }
